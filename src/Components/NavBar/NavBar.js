17363d1450a92c97e66bf88eabd8737e4fe88b73
Joe Lyons <josephjlyons90@gmail.com>
2021-05-06 13:43:34 -0400

    NavBar and css file tracking

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './NavBar.css'
+
+
+const NavBar = () => {
+    return (
+
+        <nav className='navbar navbar-dark bg-dark justify-content-center'>
+            <h5 className='navbarName'>Joseph J Lyons</h5>
+             <a class="navbar-brand" href="#">About</a>
+             <a class="navbar-brand" href="#">Portfolio</a>
+        </nav>
+
+    );
+};
+
+export default NavBar
