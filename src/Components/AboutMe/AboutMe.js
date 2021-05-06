3a9ab6e9286c3c8b61833dab920a704cada36695
Joe Lyons <josephjlyons90@gmail.com>
2021-05-06 13:43:03 -0400

    aboutMe page and css tracking

---
+++ src/Components/AboutMe/AboutMe.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import './AboutMe.css';
+
+
+const AboutMe = () => {
+    return(
+        <main class="container">
+        <div class="row" />
+            <div class="col-md-8" />
+                <h1 class="about">About Me</h1>
+                <div class="row" />
+                    <div class="col-md-12" />
+                        <div class="col-12 col-md-8 col-lg-6 float-start" />
+                           
+                     
+
+
+
+                        <p class="greetings" />
+
+                            Welcome to my page! My name is Joseph J Lyons, I am thirty-years-old.
+
+                            I recently decided to change my career path and learn how to program and code. Prior to
+                            making this decision I was a dedicated chef and well established in the restaurant
+                            community. I still enjoy cooking and can still produce some stellar dishes, but only for
+                            family and friends these days.
+
+                            In my spare time I enjoy a variety of activities. You can find me golfing, boating on Lake
+                            Winnipesaukee or watching the Boston Bruins. I enjoy spending time with friends, having some
+                            adult beverages, grilling food and playing lawn games.
+                           
+                            I decided to switch my career path for a few reasons. The physical toll of the restaurant
+                            industry is extremely taxing, and I decided that it was time to devote myself to a field
+                            with increasing opportunities for growth.
+                            
+                            I look forward to spending my time and energy learning how to write and produce functioning
+                            code. I don’t think that this change is going to come easy, but l anticipate the ability to
+                            challenge myself and work hard to have a successful career.
+
+                        
+       </main>
+    )
+}
+
+export default AboutMe;
