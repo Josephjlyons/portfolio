24c9ffe95354530b50aa7b4116178e37ff35f7c3
Joe Lyons <josephjlyons90@gmail.com>
2021-05-06 13:42:08 -0400

    app file and css tracking

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css'
+import NavBar from './Components/NavBar/NavBar'
+import Footer from './Components/Footer/Footer'
+import AboutMe from './Components/AboutMe/AboutMe'
+
+const App = () => {
+return(
+    <div>
+        <NavBar />
+        <AboutMe />
+        <Footer />
+    </div>
+)
+}
+
+export default App;
