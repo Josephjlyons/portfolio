0eb465fb60eb282bdec6bb02164de2e545756259
Joe Lyons <josephjlyons90@gmail.com>
2021-05-08 16:22:41 -0400

    update imports based on name changes |reorder comp

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
 import NavBar from './Components/NavBar/NavBar';
+import AboutMe from './Components/AboutMe/AboutMe'
 import Footer from './Components/Footer/Footer';
-import AboutMe from './Components/AboutMe/AboutMe';
+import Header from './Components/Header/Header';
 import Portfolio from './Components/PortfolioItems/Portfolio';
 
 
 const App = () => {
 return(
     <div>
-        <AboutMe />
         <NavBar />
+        <Header />
+        <AboutMe />
         <Portfolio />
         <Footer />
     </div>
