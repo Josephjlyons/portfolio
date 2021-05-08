4285f842aa7587c89b668bf83255ce8cadbd4093
Joe Lyons <josephjlyons90@gmail.com>
2021-05-08 16:23:13 -0400

    new component for about me section

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -1,102 +1,30 @@
 import React from 'react';
-import Particles from 'react-particles-js'
-import { Jumbotron, Container, Col, Row, Figure } from 'react-bootstrap'
-import './AboutMe.css';
-import photoMe from '../assets/photoMe.png'
-
-
+import { Container, Jumbotron } from 'react-bootstrap'
+import './AboutMe.css'
 
 const AboutMe = () => {
-  return (
-    <Jumbotron className='jumbo' fluid>
-      <Container>
-        <Row>
-          <Col>
-            <Particles
-              params={{
-                particles: {
-                  number: {
-                    value: 150,
-                    density: {
-                      enable: true,
-                      value_area: 3600,
-                    }
-                  },
-                  color: {
-                    value: "#F50000",
-                    animation: {
-                      enable: true,
-                      speed: 20,
-                      sync: true
-                    }
-                  },
-                  line_linked: {
-                    enable: true,
-                    distance: 150,
-                    color: '#F50000',
-                    opacity: 0.4,
-                    width: 3.5
-                  },
-                },
-              }}
-            />
-            <Figure.Image
-              width={500}
-              height={400}
-              src={photoMe}
-              alt="imageofme"
-            />
+    return (
+        <Jumbotron className='AboutJumbo' fluid>
+            <Container id='About'>
+                <h1 id='AboutMe'>About Me</h1>
+                <p> BACON
 
-          </Col>
-          <Col>
-            <Particles
-
-              params={{
-                particles: {
-                  number: {
-                    value: 150,
-                    density: {
-                      enable: true,
-                      value_area: 3700,
-                    }
-                  },
-                  color: {
-                    value: "#F50000",
-                    animation: {
-                      enable: true,
-                      speed: 20,
-                      sync: true
-                    }
-                  },
-                  line_linked: {
-                    enable: true,
-                    distance: 100,
-                    color: '#F50000',
-                    opacity: 0.4,
-                    width: 3
-                  },
-                },
-              }}
-            />
-            <h1>Joseph J Lyons</h1>
-            <a className='social' href="mailto:josephjlyons90@gmail.com"><i className="fontawesome fas fa-envelope-square"></i></a>
-
-            <a className='social' href="https://github.com/Josephjlyons"><i className=" fontawesome fab fa-github-square"></i></a>
-
-            <a className='social' href="https://www.linkedin.com/in/joseph-lyons-0a2630200/"><i className=" fontawesome fab fa-linkedin"></i></a>
-
-            <a className='social' href="tel:603-707-1045"><i className="fontawesome fas fa-phone-square-alt"></i></a>
-
-          </Col>
-        </Row>
-      </Container>
-    </Jumbotron>
-
-
-
-
-
-  )
+                Spicy jalapeno bacon ipsum dolor amet buffalo tri-tip jerky flank. Beef strip steak short ribs,
+                ribeye landjaeger spare ribs brisket pork chop shoulder tail porchetta. Boudin pork loin swine ball tip meatloaf, short loin pastrami leberkas chislic ground round tri-tip beef ribs. Boudin porchetta burgdoggen rump cupim landjaeger, short ribs filet mignon turducken ground round t-bone pork loin kevin strip steak.
+<br />
+                Spicy jalapeno bacon ipsum dolor amet buffalo tri-tip jerky flank. 
+                Beef strip steak short ribs, ribeye landjaeger spare ribs brisket pork chop shoulder tail porchetta. 
+                Boudin pork loin swine ball tip meatloaf, short loin pastrami leberkas chislic ground round tri-tip beef ribs. Boudin porchetta burgdoggen rump cupim landjaeger, short ribs filet mignon turducken ground round t-bone pork loin kevin strip steak.
+<br />
+                Pig burgdoggen pork loin swine brisket picanha sausage. 
+                Pork chop tail alcatra pork belly turducken bacon salami. 
+                Shoulder pastrami t-bone, shank drumstick burgdoggen meatloaf biltong kielbasa alcatra venison sausage flank ground round buffalo. 
+                Pig pork loin t-bone short ribs, tongue hamburger brisket tri-tip pork belly beef pork cow turducken venison leberkas. Swine leberkas kielbasa pastrami ground round chuck.
+<br />
+                </p>
+            </Container>
+        </Jumbotron>
+    );
 }
 
 export default AboutMe;
