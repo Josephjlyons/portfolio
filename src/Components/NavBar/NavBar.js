3625bba2cb74f860904385abfe45661a7327c669
Joe Lyons <josephjlyons90@gmail.com>
2021-05-08 16:25:46 -0400

    remove unused comps from react-btstrp

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-scroll';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Nav } from 'react-bootstrap';
 import './NavBar.css';
 
 
